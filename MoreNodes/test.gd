708fa44f275218cf82caff15c028d54f22d7e8a9
AgeX005 <76788560+AgeX005@users.noreply.github.com>
2021-05-18 22:09:18 -0600

    Add files via upload

--- MoreNodes/test.gd
+++ MoreNodes/test.gd
@@ -10,8 +10,12 @@ extends Node2D
 func _ready():
 	pass
 func _input(event):
+	if Input.is_action_just_pressed("ui_accept"):get_tree().paused=!get_tree().paused
 	if event is InputEventMouseButton:
 		if event.pressed:
 			var parts = ExplosionParts2D.new()
+			parts.play_on_start=false
 			add_child(parts)
 			parts.position=event.position
+			
+
